6fbb7fcdd922b6f2c4b4c15478447c27b1fb3fb7
Darley Krefta <darleykrefta@hotmail.com>
2022-05-07 01:51:08 -0300

    chore: updated dependencies and paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
+import * as path from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@helpers': path.resolve(__dirname, './src/helpers'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@entities': path.resolve(__dirname, './src/entities'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@providers': path.resolve(__dirname, './src/providers'),
+      '@services': path.resolve(__dirname, './src/services')
+    }
+  }
 })
