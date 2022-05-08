04bc47190834213f4c7849c41602d556ca96e54e
Darley Krefta <darleykrefta@hotmail.com>
2022-05-07 23:38:47 -0300

    feat: created array helpers

---
+++ src/helpers/array.ts
@@ -0,0 +1,7 @@
+export const removeAtIndex = (arr: Array<any>, index: number) => {
+  return [...arr.slice(0, index), ...arr.slice(index + 1)]
+}
+
+export const replaceAtIndex = (arr: Array<any>, index: number, newValue: any) => {
+  return [...arr.slice(0, index), newValue, ...arr.slice(index + 1)]
+}
